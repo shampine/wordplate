fa1c140ea959efa4a10fdb068fca6bc5b55c13d3
patrickshampine <patrick@patrickshampine.com>
2014-03-27 23:03:44 -0700

    Remove IE8 support with respond.js, kill old font awesome, closes #4

--- public/wp-content/themes/wpps-boilerplate/inc/wpps-functions.php
+++ public/wp-content/themes/wpps-boilerplate/inc/wpps-functions.php
@@ -1,36 +1,30 @@
-<?php 
-// Loads CSS
+<?php
 function wpps-stylesheet() {
-if ( !is_admin() ) {
+  if ( !is_admin() ) {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_register_style('bootstrap-grid', ( get_bloginfo('template_url') . '/assets/bootstrap-grid.css'));
     wp_enqueue_style('bootstrap-grid');
-    wp_register_style('font-awesome', ( get_bloginfo('template_url') . '/assets/font-awesome.min.css'));
-    wp_enqueue_style('font-awesome');
-    }
 }
 add_action('wp_enqueue_scripts','wpps-stylesheet');
 
 // Loads Javascript
 function wpps-javascript() {
-if ( !is_admin() ) {
-	wp_register_script('modernizr', ( get_bloginfo('template_url') . '/js/modernizr.min.js'));
-	wp_enqueue_script('modernizr');
-	wp_register_script('respond', ( get_bloginfo('template_url') . '/js/respond.min.js'));
-	wp_enqueue_script('respond');
+  if ( !is_admin() ) {
+    wp_register_script('modernizr', ( get_bloginfo('template_url') . '/js/modernizr.min.js'));
+    wp_enqueue_script('modernizr');
     wp_register_script('bootstrap-dropdown', ( get_bloginfo('template_url') . '/assets/bootstrap.min.js'), array('jquery'));
     wp_enqueue_script('bootstrap-dropdown');
     wp_register_script('youtube-resize', ( get_bloginfo('template_url') . '/assets/youtube.resize.js'), array('jquery')); 
     wp_enqueue_script('youtube-resize');
-	}
+  }
 }
 add_action('wp_print_scripts','wpps-javascript');
 
 // Loads Google Webfonts
 function wpps-webfonts() {
 if ( !is_admin() ) {
-    wp_register_style('webfonts', 'http://fonts.googleapis.com/css?family=Open+Sans:300,400,600');
-    wp_enqueue_style( 'webfonts');
-    }
+  wp_register_style('webfonts', 'http://fonts.googleapis.com/css?family=Open+Sans:300,400,600');
+  wp_enqueue_style( 'webfonts');
+  }
 }
 add_action('wp_print_styles','wpps-webfonts');
